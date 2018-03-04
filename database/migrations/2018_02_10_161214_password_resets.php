8ea31ae3be785fe5929ac42de71dc0f1a2154317
Sanek_OS9 <Sanek-OS9@yahoo.com>
2018-03-04 15:37:34 +0200

    update

--- database/migrations/2018_02_10_161214_password_resets.php
+++ database/migrations/2018_02_10_161214_password_resets.php
@@ -14,7 +14,7 @@ class PasswordResets extends Migration
     public function up()
     {
         Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
+            $table->string('email', 128)->index();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
