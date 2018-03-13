bc6500b645ec3e9bb176d8b6bd2db2fbe7d752c6
Sanek_OS9 <Sanek-OS9@yahoo.com>
2018-03-13 20:55:39 +0200

    views was childs for category

--- resources/views/categories/view.blade.php
+++ resources/views/categories/view.blade.php
@@ -14,8 +14,16 @@
         <section class="section extra-margins listing-section mt-2">
             <h2 class="font-bold"><strong>{{$category->name}}</strong></h2>
             <hr class="red title-hr">
+            
             <div class="row mb-4">
 
+                @foreach ($category->children()->withCount('products')->get() as $child)
+                <div class="single-post mr-2">
+                    <a href="{{route('cat.view', [$child->slug])}}" class="list-group-item list-group-item-action">
+                        <h5>{{$child->name}} <span class="badge badge-danger badge-pill">{{$child->products_count}}</span></h5>
+                    </a>
+                </div>
+                @endforeach
                 @foreach ($products as $product)
                     @include('product.inc.list', ['product' => $product])
                 @endforeach
